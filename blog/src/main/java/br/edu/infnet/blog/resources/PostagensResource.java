eb5db0c0802f145dbb85bc011c3a4f07062cfcba
mvcardoso <marcus.melo@al.infnet.edu.br>
2022-12-14 00:38:23 -0300

    Atualização

--- blog/src/main/java/br/edu/infnet/blog/resources/PostagensResource.java
+++ blog/src/main/java/br/edu/infnet/blog/resources/PostagensResource.java
@@ -2,6 +2,8 @@ package br.edu.infnet.blog.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,8 @@ import br.edu.infnet.blog.resources.dto.UsuarioDTO;
 @RequestMapping("/postagens")
 public class PostagensResource {
 	
+	private static Logger log = LoggerFactory.getLogger(PostagensResource.class);
+	
 	@Autowired
 	private RestTemplate restTemplate;
 	
@@ -32,6 +36,7 @@ public class PostagensResource {
 	
 	@PostMapping
 	public PostagensResponseDTO criarPostagem(@RequestBody PostagensDTO postagensDTO) {
+		log.info("SOLICITACAO PARA POSTAGEM: {}", postagensDTO);
 		
 		UsuarioDTO usuarioDTO = restTemplate.getForObject(usuarioApiUrl + postagensDTO.getUsuarioId(), UsuarioDTO.class);
 		
